16649c7ef5b622b3cc70612bbf7aa729e8a0b6e2
xianshenglu <xianshenglu@foxmail.com>
2019-01-30 12:54:23 +0800

    first commit

---
+++ recruiting-website-js/lagou.js
@@ -0,0 +1,5 @@
+// 拉钩 2019/1/29 19:23
+// https://www.lagou.com/jobs/list_web%E5%89%8D%E7%AB%AF?px=new&yx=10k-15k&city=%E4%B8%8A%E6%B5%B7#order
+$('.con_list_item a.position_link')
+  .toArray()
+  .forEach(e => e.click())
